b7354a409944a3bfcf80f8d360cc1d805e7692c9
Roman Hros <roman.hros@dnation.cloud>
2023-12-19 15:57:01 +0100

    Add AGE column for openstackclusterstackrelease
    Signed-off-by: Roman Hros <roman.hros@dnation.cloud>

--- api/v1alpha1/openstackclusterstackrelease_types.go
+++ api/v1alpha1/openstackclusterstackrelease_types.go
@@ -42,6 +42,7 @@ type OpenStackClusterStackReleaseStatus struct {
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
 //+kubebuilder:printcolumn:name="Ready",type="boolean",JSONPath=".status.ready"
+//+kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp",description="Time duration since creation of OpenStackClusterStackRelease"
 
 // OpenStackClusterStackRelease is the Schema for the openstackclusterstackreleases API.
 type OpenStackClusterStackRelease struct {
