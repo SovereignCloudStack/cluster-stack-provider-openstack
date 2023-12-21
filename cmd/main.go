745df81cf4a571f7b0f7f63f867a680836b0bdfe
Matej Feder <matej.feder@dnation.cloud>
2023-12-21 13:49:50 +0100

    Remove openstackclusterstackreleasetemplate controller
    Signed-off-by: Matej Feder <matej.feder@dnation.cloud>

--- cmd/main.go
+++ cmd/main.go
@@ -94,13 +94,6 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "OpenStackClusterStackRelease")
 		os.Exit(1)
 	}
-	if err = (&controller.OpenStackClusterStackReleaseTemplateReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "OpenStackClusterStackReleaseTemplate")
-		os.Exit(1)
-	}
 	if err = (&controller.OpenStackNodeImageReleaseReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
