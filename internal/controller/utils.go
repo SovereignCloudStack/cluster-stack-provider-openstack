05929709d82ab50f7a9f8af63750bd1a0db49a00
Matej Feder <matej.feder@dnation.cloud>
2023-12-27 11:58:06 +0100

    Fix formatting and error handling
    Signed-off-by: Matej Feder <matej.feder@dnation.cloud>

--- internal/controller/utils.go
+++ internal/controller/utils.go
@@ -20,23 +20,22 @@ import (
 	"context"
 	"fmt"
 
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"github.com/gophercloud/utils/openstack/clientconfig"
 	corev1 "k8s.io/api/core/v1"
-	"sigs.k8s.io/yaml"
 	"k8s.io/apimachinery/pkg/types"
-
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/yaml"
 )
 
-func getCloudFromSecret(ctx context.Context, ctrlClient client.Client, secretNamespace string, secretName string, cloudName string) (clientconfig.Cloud, error) {
+func getCloudFromSecret(ctx context.Context, ctrlClient client.Client, secretNamespace, secretName, cloudName string) (clientconfig.Cloud, error) {
 	const (
 		cloudsSecretKey = "clouds.yaml"
 	)
-	
+	var clouds clientconfig.Clouds
 	emptyCloud := clientconfig.Cloud{}
 
 	if cloudName == "" {
-		return emptyCloud, fmt.Errorf("secret name set to %v but no cloud was specified. Please set cloud_name in your machine spec", secretName)
+		return emptyCloud, fmt.Errorf("secret name set to %s but no cloud was specified. Please set cloud_name in your machine spec", secretName)
 	}
 	secret := &corev1.Secret{}
 	err := ctrlClient.Get(ctx, types.NamespacedName{
@@ -44,18 +43,20 @@ func getCloudFromSecret(ctx context.Context, ctrlClient client.Client, secretNam
 		Name:      secretName,
 	}, secret)
 	if err != nil {
-		return emptyCloud, err
+		return emptyCloud, fmt.Errorf("failed to get secret %s: %w", secretName, err)
 	}
 	content, ok := secret.Data[cloudsSecretKey]
 	if !ok {
-		return emptyCloud, fmt.Errorf("OpenStack credentials secret %v did not contain key %v",
+		return emptyCloud, fmt.Errorf("OpenStack credentials secret %s did not contain key %s",
 			secretName, cloudsSecretKey)
 	}
-	var clouds clientconfig.Clouds
 	if err = yaml.Unmarshal(content, &clouds); err != nil {
-		return emptyCloud, fmt.Errorf("failed to unmarshal clouds credentials stored in secret %v: %v", secretName, err)
+		return emptyCloud, fmt.Errorf("failed to unmarshal clouds credentials stored in secret %s: %w", secretName, err)
 	}
 
-	return clouds.Clouds[cloudName], nil
+	cloud, ok := clouds.Clouds[cloudName]
+	if !ok {
+		return emptyCloud, fmt.Errorf("failed to find cloud %s in %s", cloudName, cloudsSecretKey)
+	}
+	return cloud, nil
 }
-
