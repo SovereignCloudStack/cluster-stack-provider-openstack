2e4b5010348ba370fa809b7aeaf883803acca8fa
Michal Gubricky <michal.gubricky@dnation.cloud>
2024-02-12 09:55:41 +0100

    :sparkles: Add integration tests for both controllers (#79)
    * Add integration tests for both controllers
    
    Signed-off-by: michal.gubricky <michal.gubricky@dnation.cloud>
    
    * Update internal/test/integration/github/integration_test.go
    
    Co-authored-by: Roman Hros <roman.hros@dnation.cloud>
    Signed-off-by: Michal Gubricky <michal.gubricky@dnation.cloud>
    
    * Update internal/test/integration/github/integration_test.go
    
    Co-authored-by: Roman Hros <roman.hros@dnation.cloud>
    Signed-off-by: Michal Gubricky <michal.gubricky@dnation.cloud>
    
    * Update internal/test/integration/github/integration_test.go
    
    Co-authored-by: Roman Hros <roman.hros@dnation.cloud>
    Signed-off-by: Michal Gubricky <michal.gubricky@dnation.cloud>
    
    * Update internal/test/integration/openstack/integration_controller_test.go
    
    Co-authored-by: Roman Hros <roman.hros@dnation.cloud>
    Signed-off-by: Michal Gubricky <michal.gubricky@dnation.cloud>
    
    * Update internal/test/integration/openstack/integration_controller_test.go
    
    Co-authored-by: Roman Hros <roman.hros@dnation.cloud>
    Signed-off-by: Michal Gubricky <michal.gubricky@dnation.cloud>
    
    * Update internal/test/integration/openstack/integration_controller_test.go
    
    Co-authored-by: Roman Hros <roman.hros@dnation.cloud>
    Signed-off-by: Michal Gubricky <michal.gubricky@dnation.cloud>
    
    * Change GIT_ORG_NAME to SovereignCloudStack
    
    Signed-off-by: michal.gubricky <michal.gubricky@dnation.cloud>
    
    * Remove kind cluster logic and rename file
    
    Signed-off-by: michal.gubricky <michal.gubricky@dnation.cloud>
    
    ---------
    
    Signed-off-by: michal.gubricky <michal.gubricky@dnation.cloud>
    Signed-off-by: Michal Gubricky <michal.gubricky@dnation.cloud>
    Co-authored-by: Roman Hros <roman.hros@dnation.cloud>

---
+++ internal/test/integration/github/integration_test.go
@@ -0,0 +1,89 @@
+/*
+Copyright 2023 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package github
+
+import (
+	"github.com/SovereignCloudStack/cluster-stack-operator/pkg/test/utils"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	cspov1alpha1 "github.com/sovereignCloudStack/cluster-stack-provider-openstack/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	apiv1alpha7 "sigs.k8s.io/cluster-api-provider-openstack/api/v1alpha7"
+	clusterv1beta1 "sigs.k8s.io/cluster-api/api/v1beta1"
+)
+
+var _ = Describe("OpenStackClusterStackReleaseReconciler", func() {
+	Context("test", func() {
+		var (
+			openStackClusterStackRelease    *cspov1alpha1.OpenStackClusterStackRelease
+			testNs                          *corev1.Namespace
+			openstackClusterStackReleaseKey types.NamespacedName
+		)
+
+		BeforeEach(func() {
+			var err error
+			testNs, err = testEnv.CreateNamespace(ctx, "oscsr-integration")
+			Expect(err).NotTo(HaveOccurred())
+
+			openstackClusterStackReleaseKey = types.NamespacedName{Name: "openstack-scs-1-27-v2", Namespace: testNs.Name}
+
+			openStackClusterStackRelease = &cspov1alpha1.OpenStackClusterStackRelease{
+				TypeMeta: metav1.TypeMeta{
+					Kind:       "OpenStackClusterStackRelease",
+					APIVersion: "infrastructure.clusterstack.x-k8s.io",
+				},
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "openstack-scs-1-27-v2",
+					Namespace: testNs.Name,
+				},
+				Spec: cspov1alpha1.OpenStackClusterStackReleaseSpec{
+					CloudName: "capi-openstack-scs-1-27-v2",
+					IdentityRef: &apiv1alpha7.OpenStackIdentityReference{
+						Kind: "Secret",
+						Name: "supersecret",
+					},
+				},
+				Status: cspov1alpha1.OpenStackClusterStackReleaseStatus{
+					Ready:      false,
+					Conditions: clusterv1beta1.Conditions{},
+				},
+			}
+			Expect(testEnv.Create(ctx, openStackClusterStackRelease)).To(Succeed())
+		})
+
+		AfterEach(func() {
+			Expect(testEnv.Cleanup(ctx, testNs, openStackClusterStackRelease)).To(Succeed())
+		})
+
+		It("creates the OpenStackNodeImageRelease object", func() {
+			openstackNodeImageReleaseName := types.NamespacedName{Name: "openstack-scs-1-27-ubuntu-capi-image-v1.27.5-v2", Namespace: testNs.Name}
+			Eventually(func() error {
+				var openStackNodeImageRelease cspov1alpha1.OpenStackNodeImageRelease
+				return testEnv.Get(ctx, openstackNodeImageReleaseName, &openStackNodeImageRelease)
+			}, timeout, interval).Should(BeNil())
+		})
+
+		It("sets ClusterStackReleaseAssetsReadyCondition condition once OpenStackClusterStackRelease object is created", func() {
+			Eventually(func() bool {
+				var openStackClusterStackRelease cspov1alpha1.OpenStackClusterStackRelease
+				return utils.IsPresentAndTrue(ctx, testEnv.Client, openstackClusterStackReleaseKey, &openStackClusterStackRelease, cspov1alpha1.ClusterStackReleaseAssetsReadyCondition)
+			}, timeout, interval).Should(BeTrue())
+		})
+	})
+})
