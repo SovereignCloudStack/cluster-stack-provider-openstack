29748361dddcaf09788759854edfda8645c71bcf
Michal Gubricky <michal.gubricky@dnation.cloud>
2024-03-11 08:27:44 +0100

    :sparkles: Improve image filtering (#124)
    * Improve image filtering
    
    Signed-off-by: michal.gubricky <michal.gubricky@dnation.cloud>
    
    * Return empty string if image with ID is not found
    
    Signed-off-by: michal.gubricky <michal.gubricky@dnation.cloud>
    
    * Update internal/controller/openstacknodeimagerelease_controller.go
    
    Co-authored-by: Matej Feder <matej.feder@dnation.cloud>
    Signed-off-by: Michal Gubricky <michal.gubricky@dnation.cloud>
    
    * Fix unit-tests after added suggestion
    
    Signed-off-by: michal.gubricky <michal.gubricky@dnation.cloud>
    
    ---------
    
    Signed-off-by: michal.gubricky <michal.gubricky@dnation.cloud>
    Signed-off-by: Michal Gubricky <michal.gubricky@dnation.cloud>
    Co-authored-by: Matej Feder <matej.feder@dnation.cloud>

--- internal/controller/openstacknodeimagerelease_controller_test.go
+++ internal/controller/openstacknodeimagerelease_controller_test.go
@@ -188,15 +188,34 @@ clouds:
 	assert.NoError(t, err)
 }
 
-func TestFindImageByName(t *testing.T) {
+func TestGetImageID(t *testing.T) {
 	th.SetupHTTP()
 	defer th.TeardownHTTP()
 
 	HandleImageListSuccessfully(t)
 
-	imageName := "test_image"
+	imageFilter := &apiv1alpha1.CreateOpts{
+		ID: "123",
+	}
 
-	imageID, err := findImageByName(fakeclient.ServiceClient(), imageName)
+	imageID, err := getImageID(fakeclient.ServiceClient(), imageFilter)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "123", imageID)
+}
+
+func TestGetImageIDByNameAndTags(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	HandleImageListSuccessfully(t)
+
+	imageFilter := &apiv1alpha1.CreateOpts{
+		Name: "test_image",
+		Tags: []string{"v1"},
+	}
+
+	imageID, err := getImageID(fakeclient.ServiceClient(), imageFilter)
 
 	assert.NoError(t, err)
 	assert.Equal(t, "123", imageID)
@@ -213,33 +232,103 @@ func HandleImageListSuccessfully(t *testing.T) { //nolint: gocritic
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprintf(w, `{
 			"images": [
-				{"id": "123", "name": "test_image"},
-				{"id": "456", "name": "test_image2"},
-				{"id": "789", "name": "test_image3"}
+				{"id": "123", "name": "test_image", "tags": ["v1"]}
 			]
 		}`)
 	})
 }
 
-func TestFindImageByNameWrongImageName(t *testing.T) {
+func TestGetImageIDWithTwoSameImageNames(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	HandleImageListWithTwoImagesSuccessfully(t)
+
+	imageFilter := &apiv1alpha1.CreateOpts{
+		Name: "test_image",
+		Tags: []string{"v1"},
+	}
+
+	imageID, err := getImageID(fakeclient.ServiceClient(), imageFilter)
+
+	assert.Error(t, err) // Expecting an error due to multiple images with the same name
+	assert.Equal(t, "", imageID)
+	assert.Equal(t, err.Error(), "too many images were found with the given image name: test_image and tags: [v1]")
+}
+
+// HandleImageListWithTwoImagesSuccessfully sets up a fake response for image list request.
+func HandleImageListWithTwoImagesSuccessfully(t *testing.T) { //nolint: gocritic
+	t.Helper() // Indicate that this is a test helper function
+	th.Mux.HandleFunc("/images", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", fakeclient.TokenID)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, `{
+			"images": [
+				{"id": "123", "name": "test_image", "tags": ["v1"]},
+				{"id": "456", "name": "test_image", "tags": ["v1"]}
+			]
+		}`)
+	})
+}
+
+func TestGetImageIDNoImageFound(t *testing.T) {
+	th.SetupHTTP()
+	defer th.TeardownHTTP()
+
+	HandleImageListWithNoImageFoundSuccessfully(t)
+
+	imageFilter := &apiv1alpha1.CreateOpts{
+		Name: "test_image",
+		Tags: []string{"v1"},
+	}
+
+	imageID, err := getImageID(fakeclient.ServiceClient(), imageFilter)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "", imageID)
+}
+
+// HandleImageListWithNoImageFoundSuccessfully sets up a fake response for image list request.
+func HandleImageListWithNoImageFoundSuccessfully(t *testing.T) { //nolint: gocritic
+	t.Helper() // Indicate that this is a test helper function
+	th.Mux.HandleFunc("/images", func(w http.ResponseWriter, r *http.Request) {
+		th.TestMethod(t, r, "GET")
+		th.TestHeader(t, r, "X-Auth-Token", fakeclient.TokenID)
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintf(w, `{
+			"images": []
+		}`)
+	})
+}
+
+func TestGetImageIDWrongImageName(t *testing.T) {
 	th.SetupHTTP()
 	defer th.TeardownHTTP()
 
 	HandleImageListSuccessfully(t)
 
-	imageName := "test_bad_image"
+	imageFilter := &apiv1alpha1.CreateOpts{
+		Name: "test_bad_image",
+	}
 
-	imageID, err := findImageByName(fakeclient.ServiceClient(), imageName)
+	imageID, err := getImageID(fakeclient.ServiceClient(), imageFilter)
 
 	assert.NoError(t, err)
 	assert.NotEqual(t, "231", imageID)
 }
 
-func TestFindImageByNameNotFound(t *testing.T) {
+func TestGetImageIDNotFound(t *testing.T) {
 	th.SetupHTTP()
 	defer th.TeardownHTTP()
 
-	imageName := "test_image"
+	imageFilter := &apiv1alpha1.CreateOpts{
+		Name: "test_image",
+	}
 
 	th.Mux.HandleFunc("/images", func(w http.ResponseWriter, r *http.Request) {
 		th.TestMethod(t, r, http.MethodGet)
@@ -253,11 +342,11 @@ func TestFindImageByNameNotFound(t *testing.T) {
 
 	fakeClient := fakeclient.ServiceClient()
 
-	imageID, err := findImageByName(fakeClient, imageName)
+	imageID, err := getImageID(fakeClient, imageFilter)
 
 	assert.Error(t, err)
 	assert.Equal(t, "", imageID)
-	assert.Contains(t, err.Error(), fmt.Sprintf("failed to list images with name %s: Resource not found", imageName))
+	assert.Contains(t, err.Error(), fmt.Sprintf("failed to list images with name %s: Resource not found", imageFilter.Name))
 }
 
 // HandleImageCreationSuccessfully test setup.
