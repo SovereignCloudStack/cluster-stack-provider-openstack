bb88c99f777839c45403a9de94ad6586444a1d26
Roman Hros <roman.hros@dnation.cloud>
2024-09-10 13:49:09 +0200

    Adapt gophercloud functions and tests
    Signed-off-by: Roman Hros <roman.hros@dnation.cloud>

--- vendor/github.com/gophercloud/gophercloud/v2/openstack/image/v2/imageimport/doc.go
+++ vendor/github.com/gophercloud/gophercloud/v2/openstack/image/v2/imageimport/doc.go
@@ -1,10 +1,10 @@
 /*
 Package imageimport enables management of images import and retrieval of the
-Imageservice Import API information.
+Image service Import API information.
 
 Example to Get an information about the Import API
 
-	importInfo, err := imageimport.Get(imagesClient).Extract()
+	importInfo, err := imageimport.Get(context.TODO(), imagesClient).Extract()
 	if err != nil {
 	  panic(err)
 	}
@@ -19,7 +19,7 @@ Example to Create a new image import
 	}
 	imageID := "da3b75d9-3f4a-40e7-8a2c-bfab23927dea"
 
-	err := imageimport.Create(imagesClient, imageID, createOpts).ExtractErr()
+	err := imageimport.Create(context.TODO(), imagesClient, imageID, createOpts).ExtractErr()
 	if err != nil {
 	  panic(err)
 	}
--- vendor/github.com/gophercloud/gophercloud/v2/util.go
+++ vendor/github.com/gophercloud/gophercloud/v2/util.go
@@ -1,7 +1,7 @@
 package gophercloud
 
 import (
-	"fmt"
+	"context"
 	"net/url"
 	"path/filepath"
 	"reflect"
@@ -61,8 +61,8 @@ func NormalizeURL(url string) string {
 //
 // This is useful for determining the extra fields returned in response bodies
 // for resources that can contain an arbitrary or dynamic number of fields.
-func RemainingKeys(s interface{}, m map[string]interface{}) (extras map[string]interface{}) {
-	extras = make(map[string]interface{})
+func RemainingKeys(s any, m map[string]any) (extras map[string]any) {
+	extras = make(map[string]any)
 	for k, v := range m {
 		extras[k] = v
 	}
@@ -83,48 +83,27 @@ func RemainingKeys(s interface{}, m map[string]interface{}) (extras map[string]i
 	return
 }
 
-// WaitFor polls a predicate function, once per second, up to a timeout limit.
+// WaitFor polls a predicate function, once per second, up to a context cancellation.
 // This is useful to wait for a resource to transition to a certain state.
-// To handle situations when the predicate might hang indefinitely, the
-// predicate will be prematurely cancelled after the timeout.
 // Resource packages will wrap this in a more convenient function that's
 // specific to a certain resource, but it can also be useful on its own.
-func WaitFor(timeout int, predicate func() (bool, error)) error {
-	type WaitForResult struct {
-		Success bool
-		Error   error
+func WaitFor(ctx context.Context, predicate func(context.Context) (bool, error)) error {
+	if done, err := predicate(ctx); done || err != nil {
+		return err
 	}
 
-	start := time.Now().Unix()
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
 
 	for {
-		// If a timeout is set, and that's been exceeded, shut it down.
-		if timeout >= 0 && time.Now().Unix()-start >= int64(timeout) {
-			return fmt.Errorf("A timeout occurred")
-		}
-
-		time.Sleep(1 * time.Second)
-
-		var result WaitForResult
-		ch := make(chan bool, 1)
-		go func() {
-			defer close(ch)
-			satisfied, err := predicate()
-			result.Success = satisfied
-			result.Error = err
-		}()
-
 		select {
-		case <-ch:
-			if result.Error != nil {
-				return result.Error
+		case <-ticker.C:
+			if done, err := predicate(ctx); done || err != nil {
+				return err
 			}
-			if result.Success {
-				return nil
-			}
-		// If the predicate has not finished by the timeout, cancel it.
-		case <-time.After(time.Duration(timeout) * time.Second):
-			return fmt.Errorf("A timeout occurred")
+
+		case <-ctx.Done():
+			return ctx.Err()
 		}
 	}
 }
